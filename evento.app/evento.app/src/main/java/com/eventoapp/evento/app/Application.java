ab4c90784859e6a3a3a6c8d01497dab32b430064
DHIONSON PLASTER MOREIRA <dhionsonmoreira@hotmail.com>
2022-03-19 22:35:00 -0300

    Inicio do projeto

---
+++ evento.app/evento.app/src/main/java/com/eventoapp/evento/app/Application.java
@@ -0,0 +1,13 @@
+package com.eventoapp.evento.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
